3b54167e537ea2300b3dcc21ebfa3697df676375
snehamanojraopalekar <nehapalekar026@gmail.com>
2019-11-12 11:12:02 +0530

    [Neha]Refactor. replaced method return type as List to String Builder

--- src/main/java/com/thoughtworks/practice/CumulativePoetry.java
+++ src/main/java/com/thoughtworks/practice/CumulativePoetry.java
@@ -8,7 +8,7 @@ public class CumulativePoetry {
     private List<String> poetry = new ArrayList<>();
 
     public CumulativePoetry() {
-        poetry.add("This is the horse and the hound and the horn that belonged to");
+        poetry.add("the horse and the hound and the horn that belonged to");
         poetry.add("the farmer sowing his corn that kept");
         poetry.add("the rooster that crowed in the morn that woke");
         poetry.add("the priest all shaven and shorn that married");
@@ -22,15 +22,13 @@ public class CumulativePoetry {
         poetry.add("the house that Jack built");
     }
 
-    public List toUnfold(int revealForDay, int echo) {
-        List<String> newPoetry = new ArrayList();
+    public StringBuilder toUnfold(int revealForDay) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("This is ");
         for (int i = poetry.size() - 1; i >= poetry.size() - revealForDay; i--) {
-            int count = 1;
-            while (count <= echo) {
-                newPoetry.add(poetry.get(i));
-                count++;
+                sb.append(poetry.get(i));
+                sb.append("\n");
             }
-        }
-        return newPoetry;
+        return sb;
     }
 }
--- src/test/java/com/thoughtworks/practice/CumulativePoetryTest.java
+++ src/test/java/com/thoughtworks/practice/CumulativePoetryTest.java
@@ -2,57 +2,50 @@ package com.thoughtworks.practice;
 
 import org.junit.jupiter.api.Test;
 
-import java.util.List;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 class CumulativePoetryTest {
 
     @Test
     void givenRevealForDayAsOne_WhenUnfold_ThenShouldReturnOneLine() {
-
         CumulativePoetry cumulativePoetry = new CumulativePoetry();
-        List poetry = cumulativePoetry.toUnfold(1, 1);
-        assertEquals(1, poetry.size());
+        assertEquals("This is the house that Jack built\n",cumulativePoetry.toUnfold(1).toString());
     }
 
     @Test
     void givenRevealForDayAsTwo_WhenUnfold_ThenShouldReturnTwoLine() {
 
         CumulativePoetry cumulativePoetry = new CumulativePoetry();
-        List poetry = cumulativePoetry.toUnfold(2, 1);
-        assertEquals(2, poetry.size());
+        assertEquals("This is the house that Jack built\nthe malth that lay in\n",cumulativePoetry.toUnfold(2).toString());
     }
 
     @Test
     void givenRevealForDayAsThree_WhenUnfold_ThenShouldReturnThreeLine() {
 
         CumulativePoetry cumulativePoetry = new CumulativePoetry();
-        List poetry = cumulativePoetry.toUnfold(3, 1);
-        assertEquals(3, poetry.size());
+        assertEquals("This is the house that Jack built\nthe malth that lay in\nthe rat that ate\n", cumulativePoetry.toUnfold(3).toString());
     }
 
     @Test
     void givenRevealForDayAsTwelve_WhenUnfold_ThenShouldReturnTwelveLine() {
 
         CumulativePoetry cumulativePoetry = new CumulativePoetry();
-        List poetry = cumulativePoetry.toUnfold(12, 1);
-        assertEquals(12, poetry.size());
+        assertEquals("This is the house that Jack built\nthe malth that lay in\nthe rat that ate\nthe cat that killed\nthe dog that worried\nthat cow with the crumpled horn that tossed\nthe maiden all forlorn that milked\nthe man all tattered and torn that kissed\nthe priest all shaven and shorn that married\nthe rooster that crowed in the morn that woke\nthe farmer sowing his corn that kept\nthe horse and the hound and the horn that belonged to\n",cumulativePoetry.toUnfold(12).toString());
     }
 
-    @Test
-    void givenRevealForDayAsOneAndEchoAsThree_whenUnfold_ThenShouldReturnOneLineOneTime() {
-
-        CumulativePoetry cumulativePoetry = new CumulativePoetry();
-        List poetry = cumulativePoetry.toUnfold(2, 3);
-        assertEquals(6, poetry.size());
-    }
-
-    @Test
-    void givenRevealForDayAsFourAndEchoAsFour_whenUnfold_ThenShouldReturnOneLineOneTime() {
-
-        CumulativePoetry cumulativePoetry = new CumulativePoetry();
-        List poetry = cumulativePoetry.toUnfold(4, 4);
-        assertEquals(16, poetry.size());
-    }
+//    @Test
+//    void givenRevealForDayAsTwoAndEchoAsThree_whenUnfold_ThenShouldReturnOneLineThreeTime() {
+//
+//        CumulativePoetry cumulativePoetry = new CumulativePoetry();
+//        List poetry = cumulativePoetry.toUnfold(2, 3);
+//        assertEquals(6, poetry.size());
+//    }
+//
+//    @Test
+//    void givenRevealForDayAsFourAndEchoAsFour_whenUnfold_ThenShouldReturnOneLineOneTime() {
+//
+//        CumulativePoetry cumulativePoetry = new CumulativePoetry();
+//        List poetry = cumulativePoetry.toUnfold(4, 4);
+//        assertEquals(16, poetry.size());
+//    }
 }
